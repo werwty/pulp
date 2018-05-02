12971dc65990d219a4523247aae5889c6d928216
werwty <bihan.zh@gmail.com>
2018-05-02 14:49:52 -0400

    fdsgsdf

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env sh
+
+pip install twine
+
 pushd pulpcore
 python setup.py sdist bdist_wheel --python-tag py3
 twine upload --repository-url https://test.pypi.org/ dist/* -u PYPI_USER -p PYPI_PASSWORD
